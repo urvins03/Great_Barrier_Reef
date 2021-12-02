e33685e961d22e2051bce23847c9a487813efdc1
urvin97 <urvin.salvi40@gmail.com>
2021-12-01 23:59:14 -0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+import pickle
+import matplotlib.pyplot as plt
+import ast
+import glob
+import shutil
+import sys
+import numpy as np
+import imagesize
+import cv2
+from tqdm.notebook import tqdm
